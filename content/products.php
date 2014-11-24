d43ada8491ca581fcc83f6b78ed3cba6d32976b2
Richi Becker <richi_23becks>
2014-11-24 22:29:05 +0700

    Update Product Images
    Change product image

--- content/products.php
+++ content/products.php
@@ -6,15 +6,15 @@
 			<div class="wrap-col">
 				<div class="wrapper pad_bot2">
 					<h3><span class="dropcap">1</span>Product name</h3>
-					<figure><img src="images/page4_img1.jpg" alt=""></figure>
+					<figure><img src="images/mikei.jpg" alt=""></figure>
 					<p class="pad_bot1">bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
 					<a href="?<?php echo md5('detailproduct'); ?>" class="link1">Read More</a>
 				</div>
 				<div class="wrapper">
 					<h3><span class="dropcap">4</span>Product name</h3>
-					<figure><img src="images/page4_img2.jpg" alt=""></figure>
+					<figure><img src="images/mikei.jpg" alt=""></figure>
 					<p class="pad_bot1">bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla .</p>
-					<a href="#" class="link1">Read More</a>
+					<a href="?<?php echo md5('detailproduct'); ?>" class="link1">Read More</a>
 				</div>
 			</div>
 			</section>
@@ -22,15 +22,15 @@
 			<div class="wrap-col">
 				<div class="wrapper pad_bot2">
 					<h3><span class="dropcap">2</span>Product name</h3>
-					<figure><img src="images/page4_img3.jpg" alt=""></figure>
+					<figure><img src="images/mikei.jpg" alt=""></figure>
 					<p class="pad_bot1">bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla .</p>
-					<a href="#" class="link1">Read More</a>
+					<a href="?<?php echo md5('detailproduct'); ?>" class="link1">Read More</a>
 				</div>
 				<div class="wrapper">
 					<h3><span class="dropcap">5</span>Product name</h3>
-					<figure><img src="images/page4_img4.jpg" alt=""></figure>
+					<figure><img src="images/mikei.jpg" alt=""></figure>
 					<p class="pad_bot1">bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla .</p>
-					<a href="#" class="link1">Read More</a>
+					<a href="?<?php echo md5('detailproduct'); ?>" class="link1">Read More</a>
 				</div>
 			</div>
 			</section>
@@ -38,15 +38,15 @@
 			<div class="wrap-col">
 				<div class="wrapper pad_bot2">
 					<h3><span class="dropcap">3</span>Product name</h3>
-					<figure><img src="images/page4_img5.jpg" alt=""></figure>
+					<figure><img src="images/mikei.jpg" alt=""></figure>
 					<p class="pad_bot1">bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla .</p>
-					<a href="#" class="link1">Read More</a>
+					<a href="?<?php echo md5('detailproduct'); ?>" class="link1">Read More</a>
 				</div>
 				<div class="wrapper">
 					<h3><span class="dropcap">6</span>Product name</h3>
-					<figure><img src="images/page4_img6.jpg" alt=""></figure>
+					<figure><img src="images/mikei.jpg" alt=""></figure>
 					<p class="pad_bot1">bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla .</p>
-					<a href="#" class="link1">Read More</a>
+					<a href="?<?php echo md5('detailproduct'); ?>" class="link1">Read More</a>
 				</div>
 			</div>
 			</section>
